79164532c0c491d2ccab93b0272ea298a787b878
yuna <yuna744@gmail.com>
2024-07-22 19:14:37 +0900

    📝 add : falsy한 값, truthy한 값

---
+++ Js/WEEK_1/14.js
@@ -0,0 +1,34 @@
+// falsy한 값, truthy한 값
+
+// falsy한 값
+if (0) {
+	// main logic
+	console.log('hello1');
+}
+if ('') {
+	// main logic
+	console.log('hello2');
+}
+if (null) {
+	// main logic
+	console.log('hello3');
+}
+if (undefined) {
+	// main logic
+	console.log('hello4');
+}
+if (NaN) {
+	// main logic
+	console.log('hello5');
+}
+if (false) {
+	// main logic
+	console.log('hello6');
+}
+
+// truthy한 값
+if (true) {
+	//true
+	// main logic
+	console.log('hello7');
+}
