8b07512240ef166d9c3ac004fb7f88e60791ef7a
yuna <yuna744@gmail.com>
2024-07-23 22:13:39 +0900

    📝 add : 배열을 인수로 받아서 반복처리하는 함수 생성

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,46 @@
-//오늘 수업내용에서 무조건 이해해야 되는 내용
+/*
+  반복문 - 여러번 반복되는 특정코드를 자동반복처리
+  내장함수 - 이미 시스템에 정의되어 있는 함수
+  for(자바스크립트 기본내장함수), forEach, map(배열전용 내장함수)
+*/
 
-//1.함수에 인수 전달하고 내부의 값 리턴으로 내보내기
-function plus(num1, num2) {
-	return num1 + num2;
-}
-const result = plus(2, 3);
-console.log(result);
+//for문 패턴 :  for(초기값; 반복범위; 반복방식){반복될코드}
+/*
+  for (let i = 1; i <= 100; i++) {
+    console.log(i);
+  }
+*/
 
-//2.함수안쪽에서 조건문과 return을 활용해 함수 강제종료
-function introduce(name) {
-	if (!name) return;
-	console.log(name + '님 반갑습니다.');
+//for문으로 배열 반복처리
+/*
+  for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+  }
+*/
+
+const colors = ['red', 'green', 'blue'];
+//forEach로 반복처리
+//forEach안쪽에 함수를 인수로 전달
+//forEach안쪽에 인수로 전달되는 매개변수로는 다음의 값들이 자동 전달됨
+
+//첫번째 인수(현재 반복도는 배열의 각 데이터 값)
+//두번째 인수(현재 반복도는 순번)
+//세번째 인수(반복도는 원본 배열값 전체)
+/*
+  colors.forEach(function (data, index, arr) {
+    console.log('data', data);
+    console.log('index', index);
+    //console.log('arr', arr);
+  });
+*/
+
+const names = ['홍길동', '김판섭', '박영철'];
+//배열값을 인수로 받아서 누구님 반갑습니다 라는 콘솔문을 자동 반복해주는 함수 생성
+
+function intro(arr) {
+	arr.forEach(function (data) {
+		console.log(data + '님 반갑습니다.');
+	});
 }
-introduce('홍길동');
-introduce();
+
+intro(names);
