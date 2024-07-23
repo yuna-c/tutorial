5dea696a633dfc73db0278949ac493b77703605d
yuna <yuna744@gmail.com>
2024-07-23 22:50:59 +0900

    📝 add : tab 메뉴 기본 로직

--- Activate/js/script.js
+++ Activate/js/script.js
@@ -4,10 +4,10 @@ const boxs = main.querySelectorAll('article');
 
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', () => {
-		//직전에 모든 요소 초기화
-		btns.forEach((btn) => btn.classList.remove('on'));
-
-		//클릭한 순번에 해당하는 버튼만 활성화
+		btns.forEach((el) => el.classList.remove('on'));
 		btns[idx].classList.add('on');
+
+		boxs.forEach((el) => el.classList.remove('on'));
+		boxs[idx].classList.add('on');
 	});
 });
