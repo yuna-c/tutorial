ee3a142f723f7244c2ecce7e2fc58ab0f97442b2
yuna <yuna744@gmail.com>
2024-07-23 22:23:27 +0900

    ✅ add : important 복수개 돔요소 반복돌면서 이벤트 연결

--- Dom/script.js
+++ Dom/script.js
@@ -1,10 +1,24 @@
-const section = document.querySelector('section');
-const articles = section.querySelectorAll('article'); //유사배열
-const btn = section.querySelector('.btn');
-const childrenEl = section.children;
-const parentEl = btn.parentElement;
-const grandParentEl = btn.closest('section');
-const prevEl = btn.previousElementSibling;
-const nextEl = btn.nextElementSibling;
-const siblings = btn.parentElement.children;
-console.log(siblings);
+const btns = document.querySelectorAll('.list1 li');
+console.log(btns);
+
+/*
+  DOM (Document Object Model) : Web Api
+  : 브라우저가 HTML태그를 해석해서 렌더링을 할때 자바스크립트로 하여금 제어할수 있도록 객체형태로 만들어주는 API (Application Programing Interface)
+*/
+/*
+  btns[0].addEventListener('click', () => {
+    console.log('button1');
+  });
+  btns[1].addEventListener('click', () => {
+    console.log('button2');
+  });
+  btns[2].addEventListener('click', () => {
+    console.log('button3');
+});
+*/
+
+btns.forEach((btn, index) => {
+	btn.addEventListener('click', () => {
+		console.log(`button${index + 1}`);
+	});
+});
