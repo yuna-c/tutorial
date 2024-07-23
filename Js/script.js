42f15164fbc9f176e16b41a3b8f0a637bc426b37
yuna <yuna744@gmail.com>
2024-07-23 22:15:42 +0900

    📝 add : 템플릿 리터럴을 활용해 문자열에 변수 쉽게 삽입하기

--- Js/script.js
+++ Js/script.js
@@ -1,53 +1,12 @@
-/*
-  기존 참조형자료는 (배열, 객체)의 변수로 복사시 원본복사가 안됨 (Shallow Copy: 얕은복사, 불변성유지 안됨)
-  - 기존배열을 단순히 변수에 옮겨담을시 
-*/
-const colors = ['red', 'green', 'blue'];
-const newColors = colors;
-newColors[0] = 'hotpink';
+//문자열에 변수를 쉽게 삽입하는 법 'Template literal';
 
-//아래 콘솔 확인시 두개의 배열값 모두 변경된것을 확인
-console.log('colors', colors);
-console.log('newColors', newColors);
+intro('김영희', 20, '부산');
 
-/*
-  전개연산자 (Spread Operator)
-  - 참조형자료를 완전복사 (Deep Copy, 불변성 유지됨)
-  - heap memory에 있는 값을 물리적으로 복사
-*/
-const interests = ['game', 'movie', 'cartoon'];
-console.log(...interests);
-const newInterests = [...interests];
-newInterests[2] = 'reading';
+function intro(name, age, address) {
+	console.log('제이름은 ' + name + '이고 나이는 ' + age + '살이며 사는 곳은 ' + address + '입니다.');
+}
 
-console.log('interests', interests);
-console.log('newInterests', newInterests);
-
-const person1 = {
-	name: 'Andy',
-	age: 20,
-	address: 'seoul',
-};
-
-const person2 = { ...person1 };
-person2.age = 21;
-person2.address = 'Busan';
-
-console.log('person1', person1);
-console.log('person2', person2);
-
-//작년 회원가입정보
-const student1 = {
-	name: 'Michael',
-	age: 20,
-};
-//올해 회원가입 정보
-const extraInfo = {
-	name: 'Michael',
-	age: 21,
-	hobby: 'game',
-};
-
-//작년회원가입 정보값을 유지하면서 올해회원가입 정보값을 업데이트한 새로운 데이터 추가
-//전개연산자를 이용하면 원본을 유지하면서 기존 2개의 객체값을 합칠때 유용
-const newStudent = { ...student1, ...extraInfo };
+intro2('홍길동', 30, '서울');
+function intro2(name, age, address) {
+	console.log(`제 이름은 ${name}이고 나이는 ${age + 1}살이며 사는곳은 ${address}입니다.`);
+}
