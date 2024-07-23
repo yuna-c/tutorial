8ec34406bca37c2abbd7ded3babbe5636ab32987
yuna <yuna744@gmail.com>
2024-07-24 02:39:03 +0900

    📝 add : named export vs default export

--- Js/WEEK_2/03.js
+++ Js/WEEK_2/03.js
@@ -54,3 +54,33 @@ cris
   ${new Date()} 
 
     = 202407240210`);
+
+// named export vs default export
+
+// (5) default Export
+// name.js
+const Name = () => {};
+
+export default Name;
+
+// other file
+// 아무 이름으로 import 가능
+import newName from 'name.js';
+import NameFromOtherModule from 'name.js';
+
+// named export
+// 하나의 파일에서 여러 변수/클래스 등을 export 하는 것이 가능
+export const Name1 = () => {};
+export const Name2 = () => {};
+
+// other file
+import { Name1, Name2 } from 'name.js';
+import { newName } from 'name.js'; // x
+
+// 다른 이름으로 바꾸려면 as 사용
+import { Name1 as newName, Name2 } from 'name.js';
+
+// default export 처럼 가져오려면 * 사용
+import * as NameModule from 'name.js';
+
+console.log(NameModule.Name1);
