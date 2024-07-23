c494b63e9ac732b6264cbd80911d927d506f4888
yuna <yuna744@gmail.com>
2024-07-23 22:09:58 +0900

    📝 add : 선언적함수와 대입형함수의 호출시점 차이

--- Js/script.js
+++ Js/script.js
@@ -1,146 +1,28 @@
 /*
-  함수 (funtion)
-  : 자주쓰느 여러개의 코드들을 패키징해서 재활용 하기 위함
-  : 함수를 먼저 정의한 이후 호출 기능
-  : 함수 정의시 이름을 안붙인 함수 
-
-    => 익명함수 function(){}
-  : 이름을 붙인 함수 
-
-    => 기명함수 function name(){}
-
-  매개변수(parameter), 인수(argument)
-  : 매개변수 - 외부의 같은 함수내부로 전달하기 위한 통로명
-  : 인수 - 함수의 매개변수를 통해서 전달되는 값 자체 
+  함수의 종류
+  -익명함수: 함수에 이름이 없는 형태 (변수에 익명함수를 대입해서 주로 호출)
+  -기명함수: 함수에 이름이 있는 형태 (function 키워드로 선언하고 호출)
   
-  반환값 (return)
-  : 함수 내부에서 만들어진 값을 함수외부로 물리적으로 내보냄
-  : 함수 코드블록안에서 만들어진 값은 외부에서 접근 불가 (지역스코프)
-  
-  반환값이 필요한 이유
-  : 함수내부에서 만들어진 값이 1회성으로 소비되는 것이 아닌
-  : 추후 다른 형태로 재사용이 되야될때 함수외부로 값을 내보냄
+    -선언적함수: function 키워드로 이름을 붙여서 선언하는 형태
+    -선언적함수: 함수의 호출위치가 자유로움 (함수 선언이전에 호출가능)
+    -선언적함수가 선언되기 전에 호출가능한 이유
+    -자바스크립트 파일을 실행하기 이전에 판단단계에서 미리 변수와 선언적함수를 우선적으로 해석하기 때문 호출 가능
 
-  return의 다른 쓰임
-  : 함수내부에서 return문을 만나면 그 즉시 함수실행 종료
-  : 함수내부로직에서 에러가 발생될만한 상황이면 불필요하게 다음 코드들을 실행하지 않고 
-  : 해당 함수를 강제 종료해야 될때
+    -대입형함수: 일반 변수에 일반함수를 대입하는 형태
+    -대입형함수: 함수가 변수에 대입되는 형태이기 때문에 함수가 대입된 이후에만 호출가능
+    
+    -즉시실행함수: 익명함수 형태로 함수를 정의하자마자 자기자신을 즉시 호출하는 함수형태
 
-  true,false는 아니지만 true,false로 인지되는 값들
-  true로 인지되는 값들
-  :빈문자가 아닌 문자값
-  :0이 아닌 숫자값
-  false로 인지되는 값들
-  :빈문자값
-  :숫자 0
-  :undefined
-  :null
+  화살표함수 ES6
+  - 익명함수를 간소화처리 (내부적으로 this객체를 생성하지 않는 함수)
 */
-
-// 함수의 정의 (name이라는 [매개변수]가 연결되어 있는 함수)
-function inform(name) {
-	console.log(name + '님 하이');
-}
-
-// 함수호출 (inform함수 호출시 '홍길동'이라는 [인수] 전달)
-inform('홍길동');
-
-// 미션1 : 두개 숫자를 파라미터를 통해 인수로 전달 받아서 두개 더한 값을 콘솔로 출력하는 함수
-// typeof 변수값 : 자료형을 문자로 변환
-// 미션2 : 만약 파라미터로 전달되는 두개의 값중에 2개의 값중 하나라도 숫자가 아니면 더한 값이 아닌 숫자만 입력하세요라는 에러문구 출력 (typeof, if 활용)
-
-function plus(num1, num2) {
-	//파라미터, 매개변수
-	const plus = num1 + num2;
-
-	// 첫번째, 두번째 파라미터로 전달되는 값의 자료형을 문자열로 변수에 저장
-	const typeNum1 = typeof num1;
-	const typeNum2 = typeof num2;
-
-	console.log(typeNum1);
-	console.log(typeNum2);
-
-	// 두변수의 타입 문자값이 모두 number(숫자) 이면 두개 값을 더해서 console출력
-	if (typeNum1 === 'number' && typeNum2 === 'number') {
-		console.log(plus);
-	} else {
-		//만약 그렇지 않은 경우 에러문구 출력
-		console.log('두개의 인수를 숫자로 입력하세요');
-	}
-}
-
-plus(2, 3);
-
-//함수 호출
 console.log(plus(2, 3));
 
-function returnPlus(num1, num2) {
-	const result = num1 + num2;
-	return result;
+function plus(num1, num2) {
+	return num1 + num2;
 }
 
-//리턴값이 있는 함수 호출
-console.log(returnPlus(2, 3));
-
-//2,3을 더한값과 4,5를 더한값을 다시 더해주세요.
-const result1 = returnPlus(2, 3);
-const result2 = returnPlus(4, 5);
-console.log(result1 + result2);
-console.log(returnPlus(2, 3) + returnPlus(4, 5));
-console.log(returnPlus(2, 3) + returnPlus(4, 5));
-
-//엄청로직이 무거운 함수
-function heavyWork(data) {
-	/*
-  if (data !== undefined) {
-    console.log(data);
-    console.log(data);
-    console.log(data);
-  }
-*/
-	//에러상황이 발생되서 불필요하게 코드를 실행할 필요가 없을때
-	//return을 이용해서 강제 함수실행 중지
-	//if(data !== undefined) return;
-	if (!data) return;
-	console.log(data);
-	console.log(data);
-	console.log(data);
-}
-
-heavyWork(''); //false로 인식되는 값인 (0, '', undefined, null)이 인수로 전달되면 함수 실행 종료됨
-
-//이름값이 인수로 전달되면 인사말출력, 이름값 미전달시 경고문 (조건문, 함수)
-function introduce(name) {
-	if (name) {
-		console.log(name + '님 반갑습니다.');
-	} else {
-		console.log('사람이름을 인수로 넣어주세요.');
-	}
-}
-introduce('홍길동');
-
-//삼항연산자 (if,else문을 3개의 항을 이용해서 간소화한 형태)
-//삼항연산자를 쓰는 경우는 조건식의 true,false값에 실행할 구문의 한줄의 간단한 구문일때
-//리액트 개발에서 JSX라는 가상돔 출력구문안에는 조건문을 3항연산자만 사용가능
-// 조건식 ? 조건식이 true일떄 실행할 구문 : 조건식이 false일때 실행할 구문
-
-//삼항연산자로 고도화시킨 함수
-function introduce2(name) {
-	name ? console.log(name + '님 반갑습니다.') : console.log('사람이름을 인수로 넣어주세요.');
-}
-
-introduce('김영희');
-
-//인수값이 있을때에만 실행되는 함수를 작성시
-function test(name) {
-	if (name) console.log(name + '님 반갑습니다.');
-}
-
-function test2(name) {
-	let defaultName = '아무개';
-
-	//&&연산자뒤에 대입연산자가 들어올수 없으므로 (대입연산구문을 괄호처리해서 그룹화)
-	!name && (name = defaultName);
-	console.log(name + '님 반갑습니다.');
-}
-test2('홍길동');
+let minus = function (num1, num2) {
+	return num1 - num2;
+};
+console.log(minus(5, 2));
