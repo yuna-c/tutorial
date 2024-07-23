e9758528eec775e6403b95037729b343da6017e3
yuna <yuna744@gmail.com>
2024-07-23 22:22:18 +0900

    📝 add :  돔탐색 구문

--- Dom/script.js
+++ Dom/script.js
@@ -1,4 +1,10 @@
-let txt = 'hello';
-console.log(txt);
-const h1 = document.querySelector('h1');
-console.log(h1);
+const section = document.querySelector('section');
+const articles = section.querySelectorAll('article'); //유사배열
+const btn = section.querySelector('.btn');
+const childrenEl = section.children;
+const parentEl = btn.parentElement;
+const grandParentEl = btn.closest('section');
+const prevEl = btn.previousElementSibling;
+const nextEl = btn.nextElementSibling;
+const siblings = btn.parentElement.children;
+console.log(siblings);
