da19e6cc338e777fa091d30b9d84ac6016db1bfd
yuna <yuna744@gmail.com>
2024-07-23 22:16:28 +0900

    📝 add : 비구조화할당으로 객체나, 배열의 값들을 편하게 변수로 추출

--- Js/script.js
+++ Js/script.js
@@ -1,12 +1,37 @@
-//문자열에 변수를 쉽게 삽입하는 법 'Template literal';
+/*
+  const colors = ['red', 'green', 'blue'];
+  const color1 = colors[0];
+  const color2 = colors[1];
+  const color3 = colors[2];
+*/
 
-intro('김영희', 20, '부산');
+const [color1, color2, color3] = ['red', 'green', 'blue'];
+console.log(color1);
+console.log(color2);
+console.log(color3);
 
-function intro(name, age, address) {
-	console.log('제이름은 ' + name + '이고 나이는 ' + age + '살이며 사는 곳은 ' + address + '입니다.');
-}
+const student1 = {
+	name: 'David',
+	age: 20,
+	isFemale: false,
+};
+/*
+  const name = student1.name;
+  const age = student1.age;
+  const isFemale = student1.isFemale;
+  console.log(age);
+*/
 
-intro2('홍길동', 30, '서울');
-function intro2(name, age, address) {
-	console.log(`제 이름은 ${name}이고 나이는 ${age + 1}살이며 사는곳은 ${address}입니다.`);
-}
+const name = '홍길동';
+//뽑아낼때에는 key값으로 뽑아야 되지만 다시 다른값으로 변경처리 가능
+const { name: name1, age, isFemale } = student1;
+console.log(name1);
+console.log(age);
+console.log(isFemale);
+
+/*
+  비구조화할당, 구조분해할당 (Destructuring Assignment)
+  - 객체나 배열의 기존 구조를 분해해서 각각의 값으로 재할당
+  - 배열은 비구조화할당시 변수망 임의로 지정가능
+  - 객체는 무조건 property key값으로만 변수명 지정
+*/
