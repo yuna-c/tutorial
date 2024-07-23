d93c4ce44734613a313e00e1f498616bcceb56be
yuna <yuna744@gmail.com>
2024-07-23 22:27:17 +0900

    📝 add : 자바스크립트로 스타일 변경 및 기존 스타일값 구하기

--- Dom/script.js
+++ Dom/script.js
@@ -1,24 +1,24 @@
+/*
+  자바스크립트의 style제어
+  - 자바스크립트는 HTML,js파일외에는 접근 권한이 없음
+  - style 변경시 css파일이 변경되는 것이 아닌 HTML요소에 인라인형태로 style을 강제로 꽂아넣어서 스타일 덮어쓰기
+*/
+
 const btns = document.querySelectorAll('.list1 li');
-console.log(btns);
+const box = document.querySelector('.box');
 
-/*
-  DOM (Document Object Model) : Web Api
-  : 브라우저가 HTML태그를 해석해서 렌더링을 할때 자바스크립트로 하여금 제어할수 있도록 객체형태로 만들어주는 API (Application Programing Interface)
-*/
-/*
-  btns[0].addEventListener('click', () => {
-    console.log('button1');
-  });
-  btns[1].addEventListener('click', () => {
-    console.log('button2');
-  });
-  btns[2].addEventListener('click', () => {
-    console.log('button3');
+console.dir(box);
+
+btns[0].addEventListener('click', () => {
+	box.style.backgroundColor = 'hotpink';
+	console.dir(box);
 });
-*/
 
-btns.forEach((btn, index) => {
-	btn.addEventListener('click', () => {
-		console.log(`button${index + 1}`);
-	});
+//자스로 기존 스타일을 동적으로 덮어쓰는 것이 아닌
+//기존의 원래 스타일값을 css로 가져오는것은 불가능 (JS는 css접근 불가)
+//자바스크립트 역으로 화면에 출력되고 있는 결과물을 다시 수치적으로 재계산해서 가져옴
+
+btns[1].addEventListener('click', () => {
+	console.log(getComputedStyle(box).width);
+	console.log(getComputedStyle(box).backgroundColor);
 });
