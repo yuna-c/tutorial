a0808041240fb7ee00db004cd83f5671e406f6e0
yuna <yuna744@gmail.com>
2024-07-30 23:51:29 +0900

    📝 add : 특정 시차의 시간대 구하는 방법

--- Date/js/script.js
+++ Date/js/script.js
@@ -8,4 +8,14 @@ const hour = now.getHours(); //시간반환
 const min = now.getMinutes(); //분 반환
 const sec = now.getSeconds(); //초 반환
 const time = now.getTime(); //표준시가 지정된 시점으로부터의 밀리세컨드값이 반환
-console.log(time);
+//console.log(time);
+
+//특정 나라 표준시 구하는 방법
+//우리나라시간대를 밀리세컨드로 반환후 - 밀리세컨드 시차
+const timeGerman = time - 1000 * 60 * 60 * 8;
+
+//해당 밀리세컨드값을 new Date()의 인수로 전달
+//해당 밀리세컨드 시간값에 시간 인스턴스값이 수정되서 반환됨
+const nowGerman = new Date(timeGerman);
+console.log('한국시', now);
+console.log('독일시', nowGerman);
