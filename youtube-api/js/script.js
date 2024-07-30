5654be13b301e3d8f4f0f1f8f38b45d193cb0e9e
yuna <yuna744@gmail.com>
2024-07-30 23:45:10 +0900

    📝 add : 유튜브 데이터 호출 및 동적 DOM생성

--- youtube-api/js/script.js
+++ youtube-api/js/script.js
@@ -1,7 +1,28 @@
 const api_key = 'AIzaSyCtJt2jnOcXV6eLUZmF2gT6LGa3mSPkpbM';
 const playlist_id = 'PLYOPkdUKSFgWqafuDQN9di3uLJoTV3L3W';
 const base_url = 'https://www.googleapis.com/youtube/v3/playlistItems';
-const result_url = `${base_url}?part=snippet&api_key=${api_key}&playlistId=${playlist_id}&maxResults=5`;
+const num = 7;
+// const result_url = `${base_url}?part=snippet&api_key=${api_key}&playlistId=${playlist_id}&maxResults=5`;
+const result_url = `${base_url}?part=snippet&key=${api_key}&playlistId=${playlist_id}&maxResults=${num}`;
+
+fetch(result_url)
+	.then((data) => data.json())
+	.then((json) => {
+		console.log(json.items);
+		let tags = '';
+		json.items.forEach((el) => {
+			tags += `
+        <article>
+          <img src=${el.snippet.thumbnails.standard.url} />
+          <h2>${el.snippet.title}</h2>
+          <p>${el.snippet.description}</p>
+          <span>${el.snippet.publishedAt}</span>
+        </article>
+      `;
+		});
+
+		document.body.innerHTML = tags;
+	});
 
 /*
   브라우저 URL을 통해서 데이터 요청방식 GET방식
