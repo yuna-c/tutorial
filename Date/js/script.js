db20523ae127fc0ac6727add4dfdfefcfe18728f
yuna <yuna744@gmail.com>
2024-07-31 00:15:35 +0900

    📝 add : 전자 시계 출력

--- Date/js/script.js
+++ Date/js/script.js
@@ -1,8 +1,11 @@
 const [spanHr, spanMin, spanSec] = document.querySelectorAll('span');
-const hr = new Date().getHours();
-const min = new Date().getMinutes();
-const sec = new Date().getSeconds();
 
-spanHr.innerText = hr;
-spanMin.innerText = min;
-spanSec.innerText = sec;
+setInterval(() => {
+	const hr = new Date().getHours();
+	const min = new Date().getMinutes();
+	const sec = new Date().getSeconds();
+
+	spanHr.innerText = hr;
+	spanMin.innerText = min;
+	spanSec.innerText = sec;
+}, 1000);
