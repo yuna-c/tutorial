643fe37118f05edf86960a91497de4660000e3f8
yuna <yuna744@gmail.com>
2024-08-09 01:01:25 +0900

    📝 add : 배열(Array)

---
+++ React/WEEK_1/03.js
@@ -0,0 +1,12 @@
+/*
+배열----------------------
+*/
+
+// 1. 배열의 정의와 기본 작업
+// 1-1. 배열의 정의 : 순차 데이터 저장
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers.length); // 5
+// 1-2. 요소의 접근과 수정
+console.log(numbers[0]); //배열 접근하기
+numbers[2] = 10; //배열 수정하기
+console.log(numbers); //[1, 2, 10, 4, 5]
